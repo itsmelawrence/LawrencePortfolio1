2d161ae35df6c214fc768746b93c745bb47df7a2
Lawrence Tendenilla <lawrencetendenilla83@gmail.com>
2025-04-11 11:14:51 +0800

    Change scroll animation effect

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -401,33 +401,37 @@
             
         <script>
             // Detect if a link's href goes to the current page
-            function getSamePageAnchor (link) {
-            if (
-                link.protocol !== window.location.protocol ||
-                link.host !== window.location.host ||
-                link.pathname !== window.location.pathname ||
-                link.search !== window.location.search
-            ) {
-                return false;
-            }
+            function getSamePageAnchor(link) {
+                if (
+                    link.protocol !== window.location.protocol ||
+                    link.host !== window.location.host ||
+                    link.pathname !== window.location.pathname ||
+                    link.search !== window.location.search
+                ) {
+                    return false;
+                }
 
-            return link.hash;
+                return link.hash;
             }
 
             // Scroll to a given hash, preventing the event given if there is one
             function scrollToHash(hash, e) {
-            const elem = hash ? document.querySelector(hash) : false;
-            if(elem) {
-                if(e) e.preventDefault();
-                gsap.to(window, {scrollTo: elem, duration: 2.5});
-            }
+                const elem = hash ? document.querySelector(hash) : false;
+                if (elem) {
+                    if (e) e.preventDefault();
+                    gsap.to(window, {
+                        scrollTo: elem,
+                        duration: 2.5,
+                        ease: "power2.out" // ← Bezier-like easing
+                    });
+                }
             }
 
             // If a link's href is within the current page, scroll to it instead
             document.querySelectorAll('a[href]').forEach(a => {
-            a.addEventListener('click', e => {
-                scrollToHash(getSamePageAnchor(a), e);
-            });
+                a.addEventListener('click', e => {
+                    scrollToHash(getSamePageAnchor(a), e);
+                });
             });
 
             // Scroll to the element in the URL's hash on load
