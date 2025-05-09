2ca87fa8cfe2677ab9fa46183b4194cd83aed0c4
Lawrence <lawrencetendenilla83@gmail.com>
2025-05-10 03:56:33 +0800

    Test push 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -200,7 +200,6 @@
                     <div class="video-container">
                         <video autoplay loop muted playsinline>
                             <source src="https://lawrencebucket01.s3.ap-southeast-2.amazonaws.com/catalogtemplate.mp4" type="video/mp4">
-                        
                         </video>
                     </div>
                 </div>
