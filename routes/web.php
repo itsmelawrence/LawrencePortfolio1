0c633756f16549cb223e158f5d576590883b4e6f
Lawrence <lawrencetendenilla83@gmail.com>
2025-10-08 18:59:24 +0800

    fix: fixed redirect loop

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-// Maintenance Redirect (Active only in production + env flag)
-if (app()->environment('production') && env('SITE_UNDER_CONSTRUCTION', false)) {
-    Route::get('{any}', function () {
-        return redirect()->route('site-down');
-    })->where('any', '.*');
-}
-
-// Main routes
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/maintenance', function () {
-    return view('errors.503');
+    return view('504');
 })->name('site-down');
 
 Route::post('/', [ContactController::class, 'store'])->name('contact.us.store');
+
+// Redirect all routes to maintenance page if under construction
+if (app()->environment('production') && env('SITE_UNDER_CONSTRUCTION', false)) {
+    Route::get('{any}', function () {
+        return redirect()->route('site-down');
+    })->where('any', '^(?!maintenance).*'); // exclude the maintenance route
+}
